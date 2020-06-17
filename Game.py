6b8ac22ba2beade433e7161adaadc6cf9c55c8e4
Dewansh <56188288+dewdevil99@users.noreply.github.com>
2020-06-17 21:12:50 +0530

    Added score functionality

--- Game.py
+++ Game.py
@@ -9,7 +9,7 @@ class Game:
 	def __init__(self):
 		self.running=True
 		self.width=400
-		self.height=400
+		self.height=430
 		self.snake_segs=[]
 		self.spritesList=pygame.sprite.Group()
 
@@ -17,6 +17,12 @@ class Game:
 		pygame.init()
 		self.window=pygame.display.set_mode((self.width,self.height))
 		pygame.display.set_caption("Snake Game")
+		self.font = pygame.font.Font('freesansbold.ttf', 24)
+		self.text=self.font.render("SCORE : 0",True,pygame.Color(0,255,0))
+		self.textRect=self.text.get_rect()
+		self.textRect.x=100
+		self.textRect.y=405
+		self.score=0
 
 	def start(self):
 		self.initialize()
@@ -34,6 +40,7 @@ class Game:
 
 		while(self.running):
 			seg=None
+			
 			keys=pygame.key.get_pressed()
 			for event in pygame.event.get():
 				if(event.type==QUIT):
@@ -55,6 +62,7 @@ class Game:
 				s[0].move_down()
 
 			self.window.fill(pygame.Color(0,0,0))
+			self.window.blit(self.text,self.textRect)
 			self.food.draw_food(self.window)
 
 			if(self.food.food_eaten(s[0].rect)):
@@ -63,10 +71,12 @@ class Game:
 				#s[0].rect.x=s[0].rect.x+10
 				#snake_head.rect.y=snake_head.rect.y
 				self.snake_segs.append(seg)
+				self.score+=1
+				self.text=self.font.render("SCORE : "+str(self.score),True,pygame.Color(0,255,0))
 				self.food.new_food(s)
 				#self.spritesList.add(seg)
 				#self.snake.add_seg(self.window)
-			if(s[0].collision(s,self.width,self.height)):
+			if(s[0].collision(s,self.width,self.height-30)):
 				self.running=False
 				pygame.quit()
 				sys.exit()
