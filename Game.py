ef5c7fe0c9a540e32c8213671034e4c9c7549d64
Dewansh <56188288+dewdevil99@users.noreply.github.com>
2020-06-19 00:35:47 +0530

    Added pause functionality

--- Game.py
+++ Game.py
@@ -17,14 +17,23 @@ class Game:
 		pygame.init()
 		self.window=pygame.display.set_mode((self.width,self.height))
 		pygame.display.set_caption("Snake Game")
-		self.font = pygame.font.Font('freesansbold.ttf', 24)
-		self.scoreText=self.font.render("SCORE : 0",True,pygame.Color(0,255,0))
+		self.font_big = pygame.font.Font('freesansbold.ttf', 24)
+		self.font_small=pygame.font.Font('freesansbold.ttf', 12)
+		self.scoreText=self.font_big.render("SCORE : 0",True,pygame.Color(0,255,0))
 		self.scoreTextRect=self.scoreText.get_rect()
-		self.scoreTextRect.x=100
+		self.scoreTextRect.x=30
 		self.scoreTextRect.y=405
+		self.controlText1=self.font_small.render("p - Pause",True,pygame.Color(0,255,0))
+		self.controlText2=self.font_small.render("c - Continue",True,pygame.Color(0,255,0))
+		self.controlText1Rect=self.controlText1.get_rect()
+		self.controlText2Rect=self.controlText2.get_rect()
+		self.controlText1Rect.x=200
+		self.controlText1Rect.y=403
+		self.controlText2Rect.x=200
+		self.controlText2Rect.y=417
 
 	def setup(self):
-		self.scoreText=self.font.render("SCORE : 0",True,pygame.Color(0,255,0))
+		self.scoreText=self.font_big.render("SCORE : 0",True,pygame.Color(0,255,0))
 		self.snake_head=Snake.Snake(150,150)
 		self.snake_head.image.fill(pygame.Color(0,0,255))
 		self.spritesList.add(self.snake_head)
@@ -42,7 +51,6 @@ class Game:
 
 		while(self.running):
 			seg=None
-			
 			keys=pygame.key.get_pressed()
 			for event in pygame.event.get():
 				if(event.type==QUIT):
@@ -62,10 +70,32 @@ class Game:
 				s[0].move_up()
 			if(keys[K_DOWN]):
 				s[0].move_down()
+			if(keys[K_p]):
+				pauseText=self.font_big.render("Paused",True,pygame.Color(0,255,0))
+				pauseTextRect=pauseText.get_rect()
+				pauseTextRect.x=100
+				pauseTextRect.y=200
+				self.window.blit(pauseText,pauseTextRect)
+				pygame.display.flip()
+				l=0
+				while True:
+					for event in pygame.event.get():
+						if(event.type==QUIT):
+							self.running=False
+							pygame.quit()
+							sys.exit()
+						if(event.type==KEYDOWN):
+							if(event.key==K_c):
+								l=1
+								break
+					if(l==1):
+						break
 
 			self.window.fill(pygame.Color(0,0,0))
 			pygame.draw.line(self.window,pygame.Color(255,255,255),(0,self.height-30),(self.width,self.height-30),2)
 			self.window.blit(self.scoreText,self.scoreTextRect)
+			self.window.blit(self.controlText1,self.controlText1Rect)
+			self.window.blit(self.controlText2,self.controlText2Rect)
 			self.food.draw_food(self.window)
 
 			if(self.food.food_eaten(s[0].rect)):
@@ -73,16 +103,16 @@ class Game:
 				seg.direction=last_seg.direction
 				self.snake_segs.append(seg)
 				self.score+=1
-				self.scoreText=self.font.render("SCORE : "+str(self.score),True,pygame.Color(0,255,0))
+				self.scoreText=self.font_big.render("SCORE : "+str(self.score),True,pygame.Color(0,255,0))
 				self.food.new_food(s)
-				
+
 			if(s[0].collision(s,self.width,self.height-30)):
 				self.running=False
-				gameOverText=self.font.render("GAME OVER",True,pygame.Color(0,255,0))
+				gameOverText=self.font_big.render("GAME OVER",True,pygame.Color(0,255,0))
 				gameOverTextRect=gameOverText.get_rect()
 				gameOverTextRect.x=100
 				gameOverTextRect.y=200
-				playAgainText=self.font.render("Play Again? (Y/N)",True,pygame.Color(0,255,0))
+				playAgainText=self.font_big.render("Play Again? (Y/N)",True,pygame.Color(0,255,0))
 				playAgainTextRect=playAgainText.get_rect()
 				playAgainTextRect.x=80
 				playAgainTextRect.y=230
@@ -105,13 +135,11 @@ class Game:
 					if(l==1):
 						break
 					
-
-			
 			self.spritesList.update()
 			if seg is not None:
 				self.spritesList.add(seg)
 			self.spritesList.draw(self.window)
-			clock.tick(10)
+			clock.tick(9)
 			pygame.display.flip()
 
 game=Game()
